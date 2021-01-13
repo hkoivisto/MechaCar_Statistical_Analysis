ad11f26427a679c31bb20c4ce614cf7613b4eaed
hkoivisto <71294946+hkoivisto@users.noreply.github.com>
2021-01-12 21:32:52 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -20,3 +20,18 @@ total_summary <- mecha_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Va
 
 #create lot summary data frame
 lot_summary <- mecha_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+#use ttest to determine if the  PSI across all manufacturing lots is statistically different from the population mean of 1,500 PSI
+t.test(mecha_coil$PSI, mu=1500)
+
+#lot 1 t.test
+lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
+t.test(lot1$PSI, mu=1500)
+
+#lot 2 t.test
+lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
+t.test(lot2$PSI, mu=1500)
+
+#lot 3 t.test
+lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
+t.test(lot3$PSI, mu=1500)
