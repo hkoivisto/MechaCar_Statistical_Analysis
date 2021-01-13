0ce1157416759b93065c18a2a71ddf0e3ab04ed6
hkoivisto <71294946+hkoivisto@users.noreply.github.com>
2021-01-12 20:43:11 -0600

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,14 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #generate summary statistics 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_mpg)) 
+
+
+#Import and read Suspension_Coil.csv
+mecha_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#create summary of all coils mean,median, variance, and sd
+total_summary <- mecha_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+#summary results are different than the module shows they should be, but this calculation is accurate to my knowledge
+
+#create lot summary data frame
+lot_summary <- mecha_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
